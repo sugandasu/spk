0834bcac9aaa284b64534e6078056bc9f3280372
Arif Suganda <arifsuganda.su@gmail.com>
2020-06-17 19:44:24 +0800

    change siswa to dataset

--- dashboard/nilai/index.php
+++ dashboard/nilai/index.php
@@ -2,12 +2,12 @@
   include('../../app/settings.php');
   include('../../app/database.php');
   include('../../app/middleware.php');
-  include('../../controllers/siswa.php');
+  include('../../controllers/dataset.php');
   include('../../controllers/kriteria.php');
 
   $current_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-  $list_siswa = getSiswa($conn);
+  $list_dataset = getDataset($conn);
   $kriteria_id = $_GET['kriteria_id'];
   $kriteria = getKriteriaOne($conn, $kriteria_id);
 ?>
@@ -34,8 +34,8 @@
         <!-- Begin Page Content -->
         <div class="container-fluid">
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800"><a href="<?= $url . '/dashboard/kriteria/' ?>"></a> <?= $kriteria[1] ?> Data Penilaian Siswa</h1>
-          <p class="mb-4">Data Penilaian Siswa Calon Penerima PIP SDN Inpress 1 Kawatuna.</p>
+          <h1 class="h3 mb-4 text-gray-800"><a href="<?= $url . '/dashboard/kriteria/' ?>"></a> <?= $kriteria[1] ?> Data Penilaian Dataset</h1>
+          <p class="mb-4">Data Penilaian Dataset.</p>
           <!-- DataTales Example -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
@@ -45,38 +45,26 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
                     <tr>
-                      <th>Nis</th>
+                      <th>ID</th>
                       <th>Nama</th>
-                      <th>Tempat Lahir</th>
-                      <th>Tanggal Lahir</th>
-                      <th>Gender</th>
-                      <th>Alamat</th>
                       <th>Aksi</th>
                     </tr>
                   </thead>
                   <tfoot>
                     <tr>
-                      <th>Nis</th>
+                      <th>ID</th>
                       <th>Nama</th>
-                      <th>Tempat Lahir</th>
-                      <th>Tanggal Lahir</th>
-                      <th>Gender</th>
-                      <th>Alamat</th>
                       <th>Aksi</th>
                     </tr>
                   </tfoot>
                   <tbody>
-                    <?php if ($list_siswa->num_rows > 0) : ?>
-                    <?php while($siswa = $list_siswa->fetch_object()): ?>
+                    <?php if ($list_dataset->num_rows > 0) : ?>
+                    <?php while($dataset = $list_dataset->fetch_object()): ?>
                     <tr>
-                    <td id="nis_<?= $siswa->id ?>" data-value="<?= $siswa->nis ?>"><?= $siswa->nis ?></td>
-                    <td id="nama_<?= $siswa->id ?>" data-value="<?= $siswa->nama ?>"><?= $siswa->nama ?></td>
-                    <td id="tempat_lahir_<?= $siswa->id ?>" data-value="<?= $siswa->tempat_lahir ?>"><?= $siswa->tempat_lahir ?></td>
-                    <td id="tanggal_lahir_<?= $siswa->id ?>" data-value="<?= $siswa->tanggal_lahir ?>"><?= $siswa->tanggal_lahir ?></td>
-                    <td id="gender_<?= $siswa->id ?>" data-value="<?= $siswa->gender ?>"><?= $siswa->gender ? 'Laki-laki' : 'Perempuan' ?></td>
-                    <td id="alamat_<?= $siswa->id ?>" data-value="<?= $siswa->alamat ?>"><?= $siswa->alamat ?></td>
+                    <td id="id_<?= $dataset->id ?>" data-value="<?= $dataset->id ?>"><?= $dataset->id ?></td>
+                    <td id="nama_<?= $dataset->id ?>" data-value="<?= $dataset->nama ?>"><?= $dataset->nama ?></td>
                       <td>
-                        <a href="<?= $url ?>/dashboard/nilai-siswa?kriteria_id=<?= $kriteria_id ?>&siswa_id=<?= $siswa->id ?>" title="Detail Penilaian" id="detail" class="badge badge-primary"><i class="fa fa-paper-plane"></i></a>
+                        <a href="<?= $url ?>/dashboard/nilai-dataset?kriteria_id=<?= $kriteria_id ?>&dataset_id=<?= $dataset->id ?>" title="Detail Penilaian" id="detail" class="badge badge-primary"><i class="fa fa-paper-plane"></i></a>
                       </td>
                     </tr>
                     <?php endwhile ?>
