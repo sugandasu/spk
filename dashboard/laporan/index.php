65973ba39ec0bd40e427fc4ffee102abfd9e073b
Arif Suganda <arifsuganda.su@gmail.com>
2020-06-20 22:32:18 +0800

    create laporan

--- dashboard/laporan/index.php
+++ dashboard/laporan/index.php
@@ -2,20 +2,44 @@
   include('../../app/settings.php');
   include('../../app/database.php');
   include('../../app/middleware.php');
-  include('../../controllers/siswa.php');
+  include('../../controllers/dataset.php');
   include('../../controllers/kriteria.php');
   include('../../controllers/ranking.php');
   include('../../controllers/nilai.php');
+  include('../../controllers/smart.php');
 
 
   $current_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
   $kriteria_id = $_GET['kriteria_id'];
   $kriteria = getKriteriaOne($conn, $kriteria_id);
-  $list_siswa = getSiswa($conn);
-  $data = Topsis($conn, $kriteria_id);
+  $list_dataset = getDataset($conn);
 
+  $bobot = getKriteriaPenilaians($conn, $kriteria_id);
+  // echo "<pre>";
+  // print_r($bobot);
+  // echo "</pre>";
 
+  $smart = new Smart();
+  $bobot_baru = $smart->normasilasi_bobot($bobot);
+  // echo "<pre>";
+  // print_r($bobot_baru);
+  // echo "</pre>";
+
+  $list_nilai_dataset = getListDatasetNilai($conn, $kriteria_id);
+  // echo "<pre>";
+  // print_r($list_nilai_dataset);
+  // echo "</pre>";
+
+  $list_nilai_dataset_baru = $smart->hitung_nilai_utility($list_nilai_dataset, $bobot_baru);
+  // echo "<pre>";
+  // print_r($list_nilai_dataset_baru);
+  // echo "</pre>";
+
+  $ranking = $smart->ranking($list_nilai_dataset_baru);
+  // echo "<pre>";
+  // print_r($ranking);
+  // echo "</pre>";
 ?>
 <html>
 <head>
@@ -53,41 +77,40 @@
 			font-size: 18pt;
 			font-weight: bold;
 		}
+		th {
+			text-align: center;
+		}
+		td:first-child {
+			text-align: center;
+		}
+		td:last-child {
+			text-align: center;
+		}
 	</style>
 
 </head>
 <body>
 	<div class="header">
-		<div class="tulisan">
-			<div class="logo">
-				<img height="70px" width="70px" src="../../img/logo.png"> </img>
-			</div>
-		<div class="kop">
-		<p>PEMERINTAH KOTA PALU <br> DINAS PENDIDIKAN DAN KEBUDAYAAN</p>
-		</div>
 		<br><br>
-			      <div class="table-responsive">
-                <table class="" border="1" id="dataTableRank" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <!-- <th>No</th> -->
-                      <th>Nis</th>
-                      <th>Nama</th>
-                      <th>Hasil</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php $no=1; foreach ($data['siswa_list'] as $siswa): ?>
-                    <tr>
-                      <!-- <td><?= $no; ?></td> -->	
-                      <td><?= $siswa['nama'] ?></td>
-                      <td><?= $siswa['nis'] ?></td>
-                      <td><?= $siswa['nilai_preferensi'] ?></td>
-                    </tr>
-                    <?php $no++; endforeach ?>
-                  </tbody>
-                </table>
-              </div>
+		<div class="table-responsive">
+			<table class="" border="1" id="dataTableRank" width="100%" cellspacing="0">
+				<thead>
+				<tr>
+					<th>No</th>
+					<th>Nama</th>
+					<th>Hasil</th>
+				</tr>
+				</thead>
+				<tbody>
+				<?php $no=1; foreach ($ranking as $r): ?>
+				<tr>
+					<td><?= $no; ?></td>	
+					<td><?= $r['nama'] ?></td>
+					<td><?= $r['nilai_akhir'] ?></td>
+				</tr>
+				<?php $no++; endforeach ?>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </body>
